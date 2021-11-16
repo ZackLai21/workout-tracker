fda1282a322f656c1c9395cd4abe1c72a5864f59
Zeyang Lai <decyctus@gmail.com>
2021-11-15 20:25:05 -0800

    fixed bugs

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Workout = require('../models/Workout.js')
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
