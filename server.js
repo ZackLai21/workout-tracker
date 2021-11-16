b51df787944aff136a6b031af649cd2ba90bffba
Zeyang Lai <decyctus@gmail.com>
2021-11-15 20:23:54 -0800

    finished server.js

--- server.js
+++ server.js
@@ -1,25 +1,30 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const logger = require("morgan");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3010;
 
 const app = express();
+
+app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
 });
 
-app.use(require("./routes/apiRoutes"));
-app.use(require("./routes/htmlRoutes"));
+// routes
+ app.use(require("./routes/apiRoutes.js"));
+ app.use(require("./routes/htmlRoutes.js"));
+
 
 app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
-  });
-  
+  console.log(`Now listening ${PORT}!`);
+});
